b98ddb99b4df4487d2cccd922f661f3874140998
Kyle Drake <kyle@kyledrake.net>
2019-03-17 14:05:20 -0700

    new strategy based on splitting up paths that works better

--- case-insensitive-webserver/app.rb
+++ case-insensitive-webserver/app.rb
@@ -1,53 +1,51 @@
 # Webserver that serves local files case insensitively for serving geocities pages.
 # Uses Host to find the right directory to look in.
 # Expecting the archive directory created by the despens/Geocities scripts.
-# gem install sinatra
-# ARCHIVE_PATH=path/to/archive ruby app.rb
+# gem install bundler
+# bundle install
+# ARCHIVE_PATH=path/to/archive bundle exec ruby app.rb
 
 require 'sinatra'
+#require 'pry-byebug'
 
 raise ArgumentError, 'provide an ARCHIVE_PATH environment variable.' if ENV['ARCHIVE_PATH'].nil?
 raise ArgumentError, 'ARCHIVE_PATH does not exist' unless File.directory?(ENV['ARCHIVE_PATH'])
 
-get %r{.*} do
-  path = File.join ENV['ARCHIVE_PATH'], scrubbed_path(request.host), scrubbed_path(request.path)
-
-  # 1 - if it's a file we can see, just send it over
-  send_file path if File.file?(path)
-
-  # 2 - if it's a directory, look for an index html. If not found, pass file list.
-  if File.directory?(path)
-    index_files = Dir.entries(path).select {|f| f =~ /index\.html?/i}
-    send_file(File.join(path, index_files.first)) unless index_files.empty?
-
-    return %{<a href="..">..</a><br>} + Dir.entries(path).reject {|f| File.directory?(f)}.collect {|f| %{<a href="#{f}">#{f}</a><br>}}.join
-  end
-
-  basename = File.basename path
-  dirname  = File.dirname path
-
-  files = Dir.entries(dirname).reject { |f| File.directory?(f) }.select {|f| f =~ /#{basename}/i}
-  not_found if files.empty?
-  file_path = File.join dirname, files.first
-  send_file file_path
+before do
+  path = scrubbed_path ENV['ARCHIVE_PATH'], File.join(request.host, request.path)
+  not_found if path.nil?
+  send_file path
 end
 
-def scrubbed_path(path='')
+def scrubbed_path(archive_path, path='')
   path ||= ''
-  clean = []
+  clean = [archive_path]
 
   parts = path.to_s.split '/'
 
   parts.each do |part|
-    next if part.empty? || part == '.'
-    clean << part if part != '..'
+    next if part.empty? || part == '.' || part == '..'
+    files = Dir.entries(File.join(clean)).select {|f| f =~ /^#{part}$/i}
+    return nil if files.empty?
+    clean << files.first
   end
 
-  clean_path = clean.join '/'
+  clean_path = File.join clean
 
   # Scrub carriage garbage (everything below 32 bytes.. http://www.asciitable.com/)
   clean_path.each_codepoint do |c|
-    raise ArgumentError, 'invalid character for filename' if c < 32
+    #raise ArgumentError, 'invalid character for filename' if c < 32
+    return nil if c < 32
+  end
+
+  # Hunt for indexes if dir
+  if File.directory? clean_path
+    ['index.html', 'index.htm'].each do |fn|
+      candidate = File.join clean_path, fn
+      return candidate if File.file? candidate
+    end
+
+    return nil
   end
 
   clean_path
