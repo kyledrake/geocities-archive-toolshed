c521c5e7941a565f4eaaf1181e63e2098c9d617a
Kyle Drake <kyle@kyledrake.net>
2019-04-04 12:59:04 -0700

    phantomjs-based screenshotter

---
+++ phantomjs_screenshots/capture.rb
@@ -0,0 +1,71 @@
+# This lil' script generates the screenshots for the processed geocities archive using PhantomJS.
+# We've got millions of these go to through, so we're parallelizing the PhantomJS operations,
+# which are largely IO/wait bound. You may need to adjust the thread count.
+#
+# (deps: ruby, imagemagick)
+#
+# gem install bundler
+# bundle install
+# bundle exec ruby capture.rb
+
+if ARGV.length != 2
+  puts "usage: capture.rb base_path source_dir dest_dir"
+end
+
+require 'rubygems'
+require 'bundler/setup'
+require 'etc'
+require 'find'
+require 'concurrent-ruby'
+require 'pry-byebug'
+require 'phantomjs'
+require 'mini_magick'
+
+CORES = Etc.nprocessors
+BASE_PATH = ARGV[0] # /var/www/html
+SOURCE_DIR = ARGV[1] # www.geocities.com
+DEST_DIR = ARGV[2] # /var/www/html/screenshots
+THREAD_COUNT = 30
+
+pool = Concurrent::ThreadPoolExecutor.new(
+   min_threads: THREAD_COUNT,
+   max_threads: THREAD_COUNT,
+   max_queue: THREAD_COUNT,
+   fallback_policy: :caller_runs
+)
+
+def screenshot(url, output_path)
+  Phantomjs.run('./screenshot.js', url, output_path)
+end
+
+Find.find(ARGV[0]) do |path|
+  next unless path =~ /\.html?$/
+  next if FileTest.directory?(path)
+
+  url = path.gsub(BASE_PATH+'/', '')
+
+  output_path = File.join(DEST_DIR, url+'.png')
+
+  if File.exist?(output_path)
+    puts "skipping #{url}"
+    next
+  end
+
+  sleep 0.1 until pool.remaining_capacity > 0
+
+  puts url
+
+  pool.post do
+    Phantomjs.run './screenshot.js', 'http://'+url, output_path
+
+    # PhantomJS only makes transparent PNGs, we add a white background layer here.
+    image = MiniMagick::Image.open output_path
+    image.alpha 'remove'
+    image.alpha 'off'
+    image.format 'png'
+    image.write output_path
+  end
+end
+
+pool.shutdown
+pool.wait_for_termination
---
+++ phantomjs_screenshots/screenshot.js
@@ -0,0 +1,14 @@
+var page = require('webpage').create();
+var system = require('system');
+var args = system.args;
+
+//viewportSize being the actual size of the headless browser
+page.viewportSize = { width: 800, height: 600 };
+
+//the clipRect is the portion of the page you are taking a screenshot of
+page.clipRect = { top: 0, left: 0, width: 800, height: 600 };
+
+page.open(args[1], function(status) {
+  page.render(args[2]); //+'.jpg', {format: 'jpeg', quality: '90'}));
+  phantom.exit();
+});
