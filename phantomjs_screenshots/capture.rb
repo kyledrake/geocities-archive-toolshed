2e558144cb931b072af1a7361def668d593ad68c
Kyle Drake <kyle@kyledrake.net>
2019-04-22 01:21:02 +0000

    the current working version on the machine doing the screenshots

--- phantomjs_screenshots/capture.rb
+++ phantomjs_screenshots/capture.rb
@@ -10,50 +10,85 @@ require 'concurrent-ruby'
 require 'pry-byebug'
 require 'phantomjs'
 require 'mini_magick'
+require 'open3'
+require 'timeout'
 
 CORES = Etc.nprocessors
 BASE_PATH = ARGV[0] # /var/www/html
 SOURCE_DIR = ARGV[1] # www.geocities.com
 DEST_DIR = ARGV[2] # /var/www/html/screenshots
-THREAD_COUNT = 30
+THREAD_COUNT = CORES
+HARD_TIMEOUT = 15
 
-pool = Concurrent::ThreadPoolExecutor.new(
-   min_threads: THREAD_COUNT,
-   max_threads: THREAD_COUNT,
-   max_queue: THREAD_COUNT,
-   fallback_policy: :caller_runs
-)
+# Prime the PhantomJS install
+Phantomjs.path
 
-def screenshot(url, output_path)
-  Phantomjs.run('./screenshot.js', url, output_path)
+if File.exist?('./bad-urls.txt')	
+  $bad_urls_read = File.readlines('./bad-urls.txt')
+else
+  $bad_urls_read = []
 end
 
+# For some reason these sites behave badly, so we're moving them out of the attempts list
+$bad_urls = File.open('./bad-urls.txt', 'a')
+
+def write_bad_url(url)
+  $bad_urls.write(url+"\n")
+end
+
+$mutex = Mutex.new
+
+def out(text)
+  $mutex.synchronize { print text }
+end
+
+pool = Concurrent::ThreadPoolExecutor.new(
+  min_threads: THREAD_COUNT,
+  max_threads: THREAD_COUNT,
+  max_queue: THREAD_COUNT,
+  fallback_policy: :caller_runs
+)
+
 Find.find(ARGV[0]) do |path|
-  next unless path =~ /\.html?$/
+  begin
+    next unless path =~ /\.html?$/
+  rescue ArgumentError
+    print "SKIPPING UTF8 ERROR"
+    next
+  end
   next if FileTest.directory?(path)
 
   url = path.gsub(BASE_PATH+'/', '')
 
+  if $bad_urls_read.include?(url+"\n")
+    out "#{url}: BAD\n"
+    next
+  end
+
   output_path = File.join(DEST_DIR, url+'.png')
 
   if File.exist?(output_path)
-    puts "skipping #{url}"
+    out "#{url}: EXISTS\n"
     next
   end
 
   sleep 0.1 until pool.remaining_capacity > 0
 
-  puts url
-
   pool.post do
-    Phantomjs.run './screenshot.js', 'http://'+url, output_path
+    `timeout -k 5 #{HARD_TIMEOUT} #{Phantomjs.path} ./screenshot.js http://#{url} #{output_path}`
 
-    # PhantomJS only makes transparent PNGs, we add a white background layer here.
-    image = MiniMagick::Image.open output_path
-    image.alpha 'remove'
-    image.alpha 'off'
-    image.format 'png'
-    image.write output_path
+    if !File.exist?(output_path)
+      out "#{url}: NO IMAGE\n"
+      write_bad_url url
+    else
+      # PhantomJS only makes transparent PNGs, we add a white background layer here.
+      image = MiniMagick::Image.open output_path
+      image.alpha 'remove'
+      image.alpha 'off'
+      image.format 'png'
+      image.write output_path
+      out "#{url}: DONE\n"
+    end
   end
 end
 
--- phantomjs_screenshots/screenshot.js
+++ phantomjs_screenshots/screenshot.js
@@ -1,14 +1,21 @@
-var page = require('webpage').create();
-var system = require('system');
-var args = system.args;
+var page = require('webpage').create()
+var system = require('system')
+var args = system.args
 
 //viewportSize being the actual size of the headless browser
-page.viewportSize = { width: 800, height: 600 };
+page.viewportSize = { width: 800, height: 600 }
 
 //the clipRect is the portion of the page you are taking a screenshot of
-page.clipRect = { top: 0, left: 0, width: 800, height: 600 };
+page.clipRect = { top: 0, left: 0, width: 800, height: 600 }
+
+
+page.settings.resourceTimeout = 10000; // 10 seconds
+page.onResourceTimeout = function(e) {
+  page.render(args[2])
+  phantom.exit(1)
+};
 
 page.open(args[1], function(status) {
-  page.render(args[2]); //+'.jpg', {format: 'jpeg', quality: '90'}));
-  phantom.exit();
+  page.render(args[2]) //+'.jpg', {format: 'jpeg', quality: '90'}));
+  phantom.exit()
 });
