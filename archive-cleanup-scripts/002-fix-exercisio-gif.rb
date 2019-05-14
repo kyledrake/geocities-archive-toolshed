573e343508852476acb34d6aed8a1902c6f88781
Kyle Drake <kyle@kyledrake.net>
2019-05-13 17:23:27 -0700

    Script to fix weird filename

---
+++ archive-cleanup-scripts/002-fix-exercisio-gif.rb
@@ -0,0 +1,13 @@
+equire 'fileutils'
+
+# This particular filename is causing file traverse issues, fix.
+# It wasn't working for the web site anyways, so fix that too.
+
+if ARGV.length != 1
+  puts "usage: file.rb archive_path"
+  exit
+end
+
+fix_path = File.join ARGV[0], 'TimesSquare', 'Corridor', '1041'
+FileUtils.mv File.join(fix_path, "exerc\xEDcio.gif"), File.join(fix_path, 'exercicio.gif')
+`sed -i 's/exerc\xEDcio.gif/exercicio.gif/g' #{File.join fix_path, 'index.html'}`
